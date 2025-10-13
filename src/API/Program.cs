afc76761d5c75653290b5b724dbffaaa1ba33e1c
DESKTOP-L8PQMEN\User <power93266@naver.com>
2025-10-13 22:59:56 +0900

    feat : 데이터베이스 마이그레이션 및 CORS 설정 추가

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -14,7 +14,27 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:3000", "https://localhost:3000"));
 
 app.MapControllers();
+
+// Configure the HTTP request pipeline.
+using var scope = app.Services.CreateScope();
+
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await DbInitializer.SeedData(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
+
 
 app.Run();
