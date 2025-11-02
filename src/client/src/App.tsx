0d8e26fc1e4d889abf22990f1167c1c1829837d9
DESKTOP-L8PQMEN\User <power93266@naver.com>
2025-11-02 21:17:20 +0900

    feat: add Material-UI and Emotion for styling, update App component to use Typography, List

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,3 +1,4 @@
+import { List, ListItem, ListItemText, Typography } from "@mui/material";
 import { useEffect, useState } from "react"
 
 function App() {  
@@ -11,17 +12,19 @@ function App() {
 
     return () => {}
   }, [])
-// 구성요소에서 가져올 데이터터를 기억하는 기능이 javascript에는 없기 떄문에 무언가를 기억하려면 반응 기능에 연결해야한다 
-// 이때 후크를 useState라고한다 그럼 react 내에 저장이 가능하다  
+  // 구성요소에서 가져올 데이터터를 기억하는 기능이 javascript에는 없기 떄문에 무언가를 기억하려면 반응 기능에 연결해야한다 
+  // 이때 후크를 useState라고한다 그럼 react 내에 저장이 가능하다  
   return (    
-    <div>    
-      <h3 className="app" style={{color :'red'}}>Reactivities</h3>    
-      <ul>
-        {activities.map((activity) =>(
-          <li key={activity.id}>{activity.title}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <Typography variant="h3">Reactivities</Typography>    
+        <List>
+          {activities.map((activity) =>(
+            <ListItem key={activity.id}>
+              <ListItemText>{activity.title}</ListItemText>
+            </ListItem>
+          ))}
+        </List>
+    </>   
   )
 }
 
