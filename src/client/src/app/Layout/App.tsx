c59d0c05608eb5c4c83a23318a9737f7adfc6b4c
DESKTOP-L8PQMEN\User <power93266@naver.com>
2025-11-17 23:53:39 +0900

    style : Navigation bar 디자인 변경
    1. material 디자인을 이용하여 네비게이션 바 디자인 구성

--- src/client/src/app/Layout/App.tsx
+++ src/client/src/app/Layout/App.tsx
@@ -1,32 +1,46 @@
-import { CssBaseline, List, ListItem, ListItemText } from "@mui/material";
+import { Container, CssBaseline, List, ListItem, ListItemText } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react"
 import NavBar from "./NavBar";
 
-function App() {  
+function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
   // 구성요소라 로드될때 일어나는 일
-  useEffect(() => 
-  {    
-    axios.get<Activity[]>('https://localhost:5001/api/activities')    
-    .then(response => setActivities(response.data))    
+  useEffect(() => {
+    axios.get<Activity[]>('https://localhost:5001/api/activities')
+      .then(response => setActivities(response.data))
 
-    return () => {}
+    return () => { }
   }, [])
   // 구성요소에서 가져올 데이터터를 기억하는 기능이 javascript에는 없기 떄문에 무언가를 기억하려면 반응 기능에 연결해야한다 
   // 이때 후크를 useState라고한다 그럼 react 내에 저장이 가능하다  
-  return (    
+  return (
     <>
-    <CssBaseline/>
+      <CssBaseline />
       <NavBar />
+        {/* | 값      | 최대 너비(px) |
+        | ------ | --------- |
+        | xs     | 444       |
+        | sm     | 600       |
+        | md     | 900       |
+        | lg     | 1200      |
+        | **xl** | **1536**  | 
+
+        mt: 3 → margin-top (위쪽 마진) = 3 스페이싱 단위
+        MUI 기본 spacing 은 1 = 8px 이므로:
+        */}
+
+      <Container maxWidth='xl' sx={{ mt: 3 }}>
         <List>
-          {activities.map((activity) =>(
+          {activities.map((activity) => (
             <ListItem key={activity.id}>
               <ListItemText>{activity.title}</ListItemText>
             </ListItem>
           ))}
         </List>
-    </>   
+      </Container>
+
+    </>
   )
 }
 
--- src/client/src/app/Layout/NavBar.tsx
+++ src/client/src/app/Layout/NavBar.tsx
@@ -3,30 +3,64 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import { Container, MenuItem } from '@mui/material';
+import { Group } from '@mui/icons-material';
 
 
 export default function NavBar() {
     return (
+        // box 는 div 와 비슷한 구역을 나누는 행위 MUI의 기본 레이아웃 컨테이너.
         <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-            <Toolbar>
-            <IconButton
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="menu"
-                sx={{ mr: 2 }}
-            >
-                <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                News
-            </Typography>
-            <Button color="inherit">Login</Button>
-            </Toolbar>
-        </AppBar>
+            <AppBar position="static" sx={{
+                backgroundImage: 'linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)'
+            }}>
+                <Container maxWidth='xl'>
+                     {/* MUI Toolbar 컴포넌트에 flex 레이아웃을 적용해서 양쪽 끝으로 정렬하는 스타일 */}
+                    <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <Box>
+                            {/* flex 컨테이너로 만들고 내부 요소 간의 간격을 2 spacing(16px) 만큼 띄움. */}
+                            <MenuItem sx={{ display: 'flex', gap: 2 }}>
+                                <Group fontSize="large" />
+                                <Typography variant="h4" fontWeight='bold'>Reactivities</Typography>
+                            </MenuItem>
+                        </Box>
+                        <Box sx={{ display: 'flex' }}>
+                             {/* 
+                             | 값            | 설명                                      |
+                            | ------------ | --------------------------------------- |
+                            | `uppercase`  | 모두 대문자                                  |
+                            | `lowercase`  | 모두 소문자                                  |
+                            | `capitalize` | 단어 첫 글자만 대문자                            |
+                            | `none`       | 변환 없음 (Button 기본 대문자 스타일 끄고 싶을 때 자주 사용) |
+
+                             "uppercase" 는 문자열을 모두 대문자로 바꿈. */}
+                            <MenuItem sx={{
+                                fontSize: '1.2rem', textTransform: 'uppercase', fontWeight: 'bold'
+                            }}>
+                                Activities
+                            </MenuItem>
+                            <MenuItem sx={{
+                                fontSize: '1.2rem', textTransform: 'uppercase', fontWeight: 'bold'
+                            }}>
+                                About
+                            </MenuItem>
+                            <MenuItem sx={{
+                                fontSize: '1.2rem', textTransform: 'uppercase', fontWeight: 'bold'
+                            }}>
+                                Contact
+                            </MenuItem>
+                        </Box>
+                        <Button 
+                            size="large" 
+                            variant="contained" 
+                            color="warning"                            
+                        >
+                            Create activity
+                        </Button>
+                    </Toolbar>
+                </Container>
+
+            </AppBar>
         </Box>
     );
 }
